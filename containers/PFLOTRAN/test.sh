65e81ded39fe2db4660fd531c00d45575079088e
StefanGary <sfgary@parallelworks.com>
2022-01-21 14:51:59 +0000

    Added PFOTRAN Dockerfile

---
+++ containers/PFLOTRAN/test.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#=====================
+# Include this script
+# to test the Docker
+# build context.
+#
+# You can add other
+# tests here.
+#=====================
+echo in $0
+exit 0
