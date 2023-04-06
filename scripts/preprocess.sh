68a305923b9463747332f2666c6176b598e58476
Parallel Works app-run user <sfgary@mgmt-sfgary-cloud-00088.c.modular-magpie-167320.internal>
2023-04-06 17:55:29 +0000

    Typo in data, activate sbatch launching of colocate
    So much faster!!!

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -12,13 +12,17 @@ grd_abs_path=$3
 source ${miniconda_loc}/etc/profile.d/conda.sh
 conda activate $my_env
 
+#=============================================
 # Intake training and predict data
+#=============================================
 python prep_01_intake_train.py
 python prep_02_intake_predict.py
 
+#=============================================
 # Colocate (in parallel) using data/tools in global-river-databases
-#awk -F, -v grd=grd_abs_path ' NR>1 {system("ls -alh; echo "$2" "$3" "$4)}' prep_01_output.csv
+#=============================================
 
+#-------------Training Data----------------
 # Run colocate in header mode first to get the header
 # and ensure that the data are prepared in advance.
 prep_dir=$(pwd)
@@ -29,11 +33,79 @@ cd $prep_dir
 # Run multiple instances of run_colocate, one for each site
 
 # For now, do not run as sbatch - too many docker pulls!
-#awk -F, -v grd=${grd_abs_path} ' NR>1 {system("cd "grd"/scripts; sbatch -n 1 -c 1 --mem=0 --output=$HOME/slurm-%j.out --wrap \"./run_colocate.sh "$1" "$2" "$3"\"")}' prep_01_output.csv
+awk -F, -v grd=${grd_abs_path} ' NR>1 {system("cd "grd"/scripts; sbatch -n 1 -c 1 --mem=0 --output=$HOME/slurm-%j.out --wrap \"./run_colocate.sh "$1" "$2" "$3"\"")}' prep_01_output.csv
 
-# Run on head node to minimize docker pulls
-awk -F, -v grd=${grd_abs_path} ' NR>1 {system("cd "grd"/scripts; ./run_colocate.sh "$1" "$2" "$3" &")}' prep_01_output.csv
+# Wait for queue to empty
+n_squeue="2"
+squeue_wait=10
+while [ $n_squeue -gt 1 ]
+do
+    # Wait first - sbatch launches may take  
+    # a few seconds to register on squeue!
+    echo "Monitor waiting "${squeue_wait}" seconds..." >> prep_03_monitor.log
+    sleep $squeue_wait
+    n_squeue=$(squeue | wc -l )
+    echo "Found "${n_squeue}" lines in squeue." >> prep_03_monitor.log
+done
+echo "No more pending jobs in squeue. Proceed." >> prep_03_monitor.log
+
+# Run on head node to minimize docker pulls (add " &" for background running).
+#awk -F, -v grd=${grd_abs_path} ' NR>1 {system("cd "grd"/scripts; ./run_colocate.sh "$1" "$2" "$3)}' prep_01_output.csv
 
 # Concatenate
+# All files end up in grd_abs_path/scripts (where it works)
+# concatenate them together
+sudo cat ${grd_abs_path}/scripts/colocated.??????????.tmp >> ${grd_abs_path}/scripts/colocated.header.tmp
 
+# Archive colocation step
+cp ${grd_abs_path}/scripts/colocated.header.tmp prep_03_output_colocated_train.csv
+
+# Clean up
+rm -f ${grd_abs_path}/*.tmp
+
+echo -------------Predict Data----------------
+# Run colocate in header mode first to get the header
+# and ensure that the data are prepared in advance.
+prep_dir=$(pwd)
+cd ${grd_abs_path}/scripts
+./run_colocate.sh header 42.0 42.0
+cd $prep_dir
+
+# Run multiple instances of run_colocate, one for each site
+
+# For now, do not run as sbatch - too many docker pulls!
+awk -F, -v grd=${grd_abs_path} ' NR>1 {system("cd "grd"/scripts; sbatch -n 1 -c 1 --mem=0 --output=$HOME/slurm-%j.out --wrap \"./run_colocate.sh "$1" "$2" "$3"\"")}' prep_02_output.csv
+
+# Wait for queue to empty
+n_squeue="2"
+squeue_wait=10
+while [ $n_squeue -gt 1 ]
+do
+    # Wait first - sbatch launches may take 
+    # a few seconds to register on squeue!
+    echo "Monitor waiting "${squeue_wait}" seconds..." >> prep_04_monitor.log
+    sleep $squeue_wait
+    n_squeue=$(squeue | wc -l )
+    echo "Found "${n_squeue}" lines in squeue." >> prep_04_monitor.log
+done
+echo "No more pending jobs in squeue. Proceed." >> prep_04_monitor.log
+
+# Run on head node to minimize docker pulls
+#awk -F, -v grd=${grd_abs_path} ' NR>1 {system("cd "grd"/scripts; ./run_colocate.sh "$1" "$2" "$3" &")}' prep_02_output.csv
+
+# Concatenate
+# All files end up in grd_abs_path/scripts (where it works)
+# concatenate them together
+sudo cat ${grd_abs_path}/scripts/colocated.??????????.tmp >> ${grd_abs_path}/scripts/colocated.header.tmp
+
+# Archive colocation step
+cp ${grd_abs_path}/scripts/colocated.header.tmp prep_04_output_colocated_predict.csv
+
+# Clean up
+rm -f ${grd_abs_path}/*.tmp
+
+#=============================================
 # Paste
+#=============================================
+
+#-------------Training Data----------------
