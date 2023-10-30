01e79fb11adc0031652154717d836b06144d0e2f
Parallel Works app-run user <sfgary@mgmt-sfgary-cloud-00265.c.modular-magpie-167320.internal>
2023-10-30 20:03:47 +0000

    00081 on Mon Oct 30 20:03:47 UTC 2023

--- ml_models/sl_2/superlearner_conf_TTR_QT.py
+++ ml_models/sl_2/superlearner_conf_TTR_QT.py
@@ -517,7 +517,7 @@ SuperLearnerConf = {
                     "regressor__etr__n_estimators": (100, 10000),
                     "regressor__etr__ccp_alpha": [0, 0.001, 0.01, 0.1],
                     "regressor__etr__max_features": [0.1, 0.3, 0.5, 0.8, 1.0],
-                    "regressor__etr__criterion": ["squared_error", "absolute_error", "friedman_mse", "poisson"],
+                    "regressor__etr__criterion": ["squared_error", "absolute_error", "friedman_mse"],
                     "regressor__etr__max_depth": [2, 3, 4, 5, 6, 7, 8],
                     "regressor__etr__min_samples_split": [0.1, 0.2, 0.3],
                     "regressor__etr__min_samples_leaf": [0.1, 0.2, 0.3]
